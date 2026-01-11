614a694ea8200dd3df204b14e2b99e4d5228b9ce
Jeffrey Goodman <63065198+jtgoods@users.noreply.github.com>
2026-01-11 15:46:53 -0800

    Add function to compute pay based on hours and rate

---
+++ build_functions.py
@@ -0,0 +1,12 @@
+def computepay(h, r):
+    if h > 40:
+       pay = (40 * r) + ((h - 40) * r * 1.5) 
+       return pay
+    else:
+        pay = h * r
+        return pay
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+p = computepay(hrs, rate)
+print("Pay", p)
